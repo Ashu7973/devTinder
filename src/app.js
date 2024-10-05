1ffbfe92ba3df828e3992f92732ef2c865e638b4
Ashu7973 <ashutoshjha7973@gmail.com>
2024-10-06 00:33:37 +0530

    add demo api link

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 
 const app = express();
+app.use((req, res, next) => {
+    res.send("Hello World");
+})
 app.listen(3000,(req, res)=>{
     console.log("server is running on port 3000");
 })
